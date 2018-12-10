7ca5efcdc3c7e4e27af35302be9d8d8453ba0e4b
Andrew <andrew.gcorley@gmail.com>
2018-12-09 17:52:50 -0700

    added selection

--- CPSC_481_Trailexplorers/HikeItem.xaml.cs
+++ CPSC_481_Trailexplorers/HikeItem.xaml.cs
@@ -24,7 +24,10 @@ namespace CPSC_481_Trailexplorers
     /// </summary>
     public partial class HikeItem : UserControl
     {
+        System.Windows.Media.Brush brush2 = null;
+
         public HikeItem()
+            
         {
             InitializeComponent();
           
@@ -57,11 +60,39 @@ namespace CPSC_481_Trailexplorers
             background.Source = bi3;
             background.Stretch = Stretch.UniformToFill;
             var brush = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, (byte)(x.R*(3.0/4.0)), (byte)(x.G*(3.0/4.0)), (byte)(x.B * (3.0 / 4.0))));
+            brush2 = brush;
             //var converter = new System.Windows.Media.BrushConverter();
             //var brush = (System.Windows.Media.Brush)converter.ConvertFromString(x.ToString());
             //System.Windows.Media.Color darkBrush = System.Windows.Forms.ControlPaint.Dark(brush);
             backgroundGrid.Background = brush;
             
+        }
+
+        private void Viewbox_MouseEnter(object sender, MouseEventArgs e)
+        {
+          backgroundGrid.Background = new SolidColorBrush(Colors.ForestGreen);
+        }
+
+        private void Viewbox_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+            backgroundGrid.Background = brush2;
+
+          
+            //BitmapImage bi3 = new BitmapImage();
+            //bi3.BeginInit();
+            //bi3.UriSource = new Uri("HikeProfileimages/" + parkNameDisplayLabel.Content.ToString() + ".jpg", UriKind.Relative);
+            //bi3.EndInit();
+
+            //Bitmap b = new Bitmap("../../HikeProfileimages/" + parkNameDisplayLabel.Content.ToString() + ".jpg");
+            //System.Drawing.Color x = b.GetPixel(540, 200);
+
+
+            //myimage.Source = bi3;
+            //System.Diagnostics.Debug.WriteLine(background.Source.ToString());
+            //background.Source = bi3;
+            //background.Stretch = Stretch.UniformToFill;
+            //var brush = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, (byte)(x.R * (3.0 / 4.0)), (byte)(x.G * (3.0 / 4.0)), (byte)(x.B * (3.0 / 4.0))));
         }
     }
 }
